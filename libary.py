9277516310b76bce34de6c3034bc0e1621ab693c
Jessica <basnetjessica003@gmail.com>
2025-12-04 20:14:37 +0545

    add change

--- libary.py
+++ libary.py
@@ -20,21 +20,24 @@ if not os.path.exist('book.txt'):
     with open('book.txt','w')as f:
 
 ### load data from the file
-def load_user():
-    """ load all the user from the user.txt into dictonary"""
-    user_dict ={}
+     def load_user():  
+         """ load all the user from the user.txt into dictonary"""
+    users_dict ={}
 
     try:
         with open('user.txt','r')as f:
             for line in f:
                 line = line.strip()
                 if line:
-                    username,password = password
+                    username,password = line.split(',')
+                    users_dict[username]= password
+
     except FileNotFoundError:
 
         print("file not found!")
-
-    return user_dict  
+        
+        return users_dict  
+ #book_id,title,author,quantity
 
 def load_books():
     books_list = []
@@ -60,8 +63,8 @@ def load_books():
 
 
     def get_existing_books_id(books_list): 
-        """create a set to store all the ids pf the books"""
-        book_ids + set()
+        """create a set to store all the ids of the books"""
+        book_ids = set()
         for book in books_list:
             #dictionary
             book_ids.add(book['id'])
@@ -71,10 +74,27 @@ def load_books():
     ###user registration
     def register_user(user_dict):
         """register a new user""" 
-        print("\
-              --- Register a New user ---") 
+        print("\n --- Register a New user ---") 
         username = input("enter the username:").strip()
         password = input("enter the password:").strip()    
+if username in users_dict:
+        print(f"username alrealy exists!")
+        return False
+        if not username or not password:
+         print("Username and password cannot be empty")
+        return False
+        users_dict[username] = password
+
+    # save the registered user to the file 'users.txt'
+        with open('users.txt', 'a') as f:
+         f.write(f"{username},{password}\n")
+
+        print("registration successfull!")
+        return True
+
+users_dict = load_user()
+print(users_dict)
+register_user(users_dict)
 
 
 
